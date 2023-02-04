5c298e0577c52027fc827b3b4efeeb100ed84624
dongha <kimdongha15@naver.com>
2023-02-04 15:12:45 +0900

    move apikey to xml file

--- src/main/java/com/chatgpt/demo/controller/GptController.java
+++ src/main/java/com/chatgpt/demo/controller/GptController.java
@@ -5,11 +5,9 @@ import com.chatgpt.demo.model.RequestMakeNameVo;
 import com.chatgpt.demo.model.ResponseVo;
 import com.chatgpt.demo.service.GptService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 
--- src/main/java/com/chatgpt/demo/service/GptService.java
+++ src/main/java/com/chatgpt/demo/service/GptService.java
@@ -3,6 +3,8 @@ package com.chatgpt.demo.service;
 import com.chatgpt.demo.model.ReqeustQuestionVo;
 import com.chatgpt.demo.model.RequestMakeNameVo;
 import com.chatgpt.demo.model.ResponseVo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -13,8 +15,10 @@ import java.util.List;
 import java.util.Map;
 
 @Service
+@PropertySource("classpath:apikey.properties")
 public class GptService {
-    private static final String API_KEY = "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";
+    @Value("${apikey}")
+    private String API_KEY;
     private static final String ENDPOINT = "https://api.openai.com/v1/completions";
     public ResponseVo getVariableName(RequestMakeNameVo requestMakeNameVo) {
         ResponseVo responseVo = new ResponseVo();
@@ -41,13 +45,8 @@ public class GptService {
 
     public ResponseVo getClassName(RequestMakeNameVo requestMakeNameVo) {
         ResponseVo responseVo = new ResponseVo();
-        String caseName = "";
         try {
-            if(!ObjectUtils.isEmpty(requestMakeNameVo.getCaseName())){
-                caseName = "Please make it into a " + requestMakeNameVo.getCaseName() + " case";
-            }
-
-            String question = requestMakeNameVo.getText() + "(을)를 지칭하는 클래스 명으로 뭐가 적절할까요? " + caseName;
+            String question = requestMakeNameVo.getText() + "(을)를 지칭하는 클래스 명으로 뭐가 적절할까요?";
 
             String answer = generateText(question, 0.5f, 100);
 
